347d32ad30e500a5f03f0be7ffe1ff0631b2229e
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-21 15:02:15 -0400

    Added typewriter functions

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 
 var i = 0;
-var txt = 'This is some random text maynnnneee';
+var txt =  "Hi! <br> My Name is Shayanne. <br> Welcome to my portfolio page. \n A little about me \n I am a graduate of the University of Ottawa and Sheridan College.\n I began my professional career as a casting director and production coordinator on tv and commercial productions in Toronto, Canada.\n During the pandemic of 2020 I decided it was time to learn some new skills. I’ve always been interested in coding/programming so I figured I would take the down time I had and further develop on those interests. Since May of 2020, I’ve completed several interesting programming projects and learned many aspects of front end and back end programming. I’m currently on my way to becoming a full stack developer.\n In my down time I watch movies, play with my dog and take photos. Follow me on IG if you wanna see some pics."
 var speed = 60;
 
 function typeWriter() {
@@ -10,3 +10,5 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+
