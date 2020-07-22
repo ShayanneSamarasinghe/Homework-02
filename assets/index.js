5a79bd92c4c1de6671fa256bd4d85862a903d57b
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-22 01:39:17 -0400

    Fixes and problems

--- assets/index.js
+++ assets/index.js
@@ -1,32 +1,34 @@
 
 var i = 0;
-var txt =  "Hi! ** My Name is ${Shayanne}. ** Welcome to my portfolio page. ** A little about me: ** I am a graduate of the ${University of Ottawa} and ${Sheridan College}.** I began my professional career as a ${casting director} and ${production coordinator} on tv and commercial productions in ${Toronto, Canada}.** During the pandemic of 2020 I decided it was time to learn some new skills. I’ve always been interested in ${coding/programming} so I figured I would take the down time I had and further develop on those interests. Since May of 2020, I’ve completed several interesting programming projects and learned many aspects of front end and back end programming. I’m currently on my way to becoming a full stack developer.** In my down time I [watch movies, play with my dog and take photos] Follow me on IG if you wanna see some pics."
+var txt =  "Hi! ** My Name is ${Shayanne}. ** Welcome to my portfolio page. ** A little about me: ** I am a graduate of the ${University of Ottawa} and ${Sheridan College}.** I began my professional career as a ${casting director} and ${production coordinator} on tv and commercial productions in ${Toronto, Canada}.** During the pandemic of 2020 I decided it was time to learn some new skills. I’ve always been interested in ${coding/programming} so I figured I would take the downtime I had and further build on that curiosity. Since May of 2020, I’ve completed several interesting programming projects and learned many aspects of front end and back end programming. I’m currently on my way to becoming a full stack developer.** In my downtime I [watch movies, play with my dog and take photos]."
 var speed = 60;
 var newLine = "<br>"
-
-// function typeWriter() {
-//   if (i < txt.length) {
-//     document.getElementById("indexJumbotron").innerHTML += txt.charAt(i);
-//     i++;
-//     setTimeout(typeWriter, speed);
-//   }
-// }
+var spanOpen =  "<span class='templateLiteral'>"
+var spanClose = "</span>"
+var spanOpen =  "<span class='array'>"
+var spanClose = "</span>"
 
 
 
 function typeWriter() {
-  console.log("Something again")
     if (i < txt.length) {
-      console.log("something something again")
       if(txt.charAt(i) === "*"){
-        console.log("something")
         document.getElementById("indexJumbotron").innerHTML += newLine;
         i+=2
-      }else{
+      }else if (txt.charAt(i) === "$"){
+      const end = txt.indexOf("}", i) + 1;
+      const span = spanOpen + txt.substring(i, end) + spanClose
+      document.getElementById("indexJumbotron").innerHTML += span;
+      i = end;
+    }else if (txt.charAt(i) === "["){
+      const end = txt.indexOf("]", i) + 1;
+      const span = spanOpen + txt.substring(i, end) + spanClose
+      document.getElementById("indexJumbotron").innerHTML += span;
+      i = end;
+    }else{
       document.getElementById("indexJumbotron").innerHTML += txt.charAt(i);
       i++;
-      
-      }
+    }
     setTimeout(typeWriter, speed);
   }
 }
--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,13 +1,34 @@
 
 var i = 0;
-var txt = 'This is some random text maynnnneee';
+var txt =  "This is my portfolio ** ** Please check out my group projects ${nutShell} and ${Page Exchange} ** **  "
 var speed = 60;
+var newLine = "<br>"
+var spanOpen =  "<span class='templateLiteral>"
+var spanClose = "</span>"
+var spanOpen =  "<span class='array'>"
+var spanClose = "</span>"
 
-function typeWriter() {
-  console.log("something something")
-  if (i < txt.length) {
-    document.getElementById("portfolioJumbotron").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+
+
+function typeWriter2() {
+    if (i < txt.length) {
+      if(txt.charAt(i) === "*"){
+        document.getElementById("portfolioJumbotron").innerHTML += newLine;
+        i+=2
+      }else if (txt.charAt(i) === "$"){
+      const end = txt.indexOf("}", i) + 1;
+      const span = spanOpen3 + txt.substring(i, end) + spanClose3
+      document.getElementById("portfolioJumbotron").innerHTML += span;
+      i = end;
+    }else if (txt.charAt(i) === "["){
+      const end= txt.indexOf("]", i) + 1;
+      const span = spanOpen4 + txt.substring(i, end) + spanClose4
+      document.getElementById("portfolioJumbotron").innerHTML += span;
+      i = end;
+    }else{
+      document.getElementById("portfolioJumbotron").innerHTML += txt.charAt(i);
+      i++;
+    }
+    setTimeout(typeWriter2, speed);
   }
 }
