dc72574d680a976ab0a0794c5745f99b97fad728
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-22 10:14:52 -0400

    Tweaks

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -3,14 +3,14 @@ var i = 0;
 var txt =  "This is my portfolio ** ** Please check out my group projects ${nutShell} and ${Page Exchange} ** **  "
 var speed = 60;
 var newLine = "<br>"
-var spanOpen =  "<span class='templateLiteral2>"
+var spanOpen =  "<span class='templateLiteral>"
 var spanClose = "</span>"
-var spanOpen2 =  "<span class='array2'>"
+var spanOpen2 =  "<span class='array'>"
 var spanClose2 = "</span>"
 
 
 
-function typeWriter2() {
+function typeWriter() {
     if (i < txt.length) {
       if(txt.charAt(i) === "*"){
         document.getElementById("portfolioJumbotron").innerHTML += newLine;
@@ -22,13 +22,13 @@ function typeWriter2() {
       i = end;
     }else if (txt.charAt(i) === "["){
       const end= txt.indexOf("]", i) + 1;
-      const span = spanOpen2 + txt.substring(i, end) + spanClose2
+      const span = spanOpen + txt.substring(i, end) + spanClose
       document.getElementById("portfolioJumbotron").innerHTML += span;
       i = end;
     }else{
       document.getElementById("portfolioJumbotron").innerHTML += txt.charAt(i);
       i++;
     }
-    setTimeout(typeWriter2, speed);
+    setTimeout(typeWriter, speed);
   }
 }
